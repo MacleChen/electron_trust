909016f2532b6b99c751086709ee5ea5c84bf1e7
陈帆 <chenfan@chenfans-MacBook-Air.local>
2024-10-29 18:34:27 +0800

    添加discover中tabs的部分页面功能

--- src/views/discover/DiscoverTabsView.vue
+++ src/views/discover/DiscoverTabsView.vue
@@ -2,22 +2,50 @@
     <div>
         <van-nav-bar title="3 tabs" @click-left="navBarLeftClick" >
         <template #left>
-            <img src="../../assets/asserts/arrow-left-f_Normal@2x.png" style="height: 24px; width: 24px;" />
+            <img src="../../assets/asserts/close-f_Normal@2x.png" style="height: 24px; width: 24px;" />
+        </template>
+        <template #right>
+            <img src="../../assets/asserts/delete-f_Normal@2x.png" style="height: 24px; width: 24px;" />
         </template>
     </van-nav-bar> 
-    <div style="padding-bottom: 50px;">
-        <ul v-for="(item, index) in settingList" :key="item.title" @click="settingCellClick(index)">
-        <div class="setting_cell_containter">
-            <div style="width: 20px; height: 20px;">
-                <img @dragstart.prevent :src="item.imgStr" width="100%" />
-            </div> 
-            <div style="flex-grow: 1; display: flex; justify-content: center; align-items: center;">
-                <label style="font-weight: bold; flex: 1; text-align: left; margin-left: 10px;">{{ item.title }}</label> 
-                <img style="width: 5px; height: 10px; margin-right: 20px;" src="../../assets/asserts/Arrow Right (1)_Normal@3x.png"/>
+    
+    <van-grid :border="false" :column-num="2">
+    <van-grid-item>
+        <div style="height: 200px; background-color: lightslategray;">
+            <div style="height: 50px; display: flex; align-items: center;">
+                <div class="item_content_cell_style" style="width: 50%; justify-content: left;">
+                    <img style="margin-left: 5px;" src="../../assets/asserts/viacoin_Normal.png" width="15px" height="15px"/>
+                    <div style="margin-left: 10px; ">
+                        <div>
+                            <label style="font-size: 10px; font-weight: bold; color: black;">four.meme</label>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="item_content_cell_style" style="width: 50%; justify-content: flex-end;">
+                    <img style="margin-left: 5px;" src="../../assets/asserts/viacoin_Normal.png" width="15px" height="15px"/>
+                </div>
+            </div>
+
+
+            <div>
+
             </div>
         </div>
-        </ul>
-    </div>
+    </van-grid-item>
+    <van-grid-item>
+        <van-image
+        src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg"
+        />
+    </van-grid-item>
+    <van-grid-item>
+        <van-image
+        src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-3.jpeg"
+        />
+    </van-grid-item>
+    </van-grid>
+
     </div>
     
 </template>
